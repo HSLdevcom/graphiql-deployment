0ee60a1040201c396e1aacb6bfc18188f5b48894
Joona Ojapalo <joona.ojapalo@cgi.fi>
2022-08-15 11:57:49 +0300

    fix: port prettify & history to graphiql 1.x

--- src/GraphiQL.js
+++ src/GraphiQL.js
@@ -1,35 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 import { Route, withRouter } from 'react-router-dom';
-import GraphiQL, {
-  ToolbarSelect,
-  ToolbarSelectOption,
-  ToolbarButton
-} from 'graphiql';
+
+import GraphiQL from 'graphiql';
 import { usePrettifyEditors, useHistoryContext } from '@graphiql/react';
 import 'graphiql/graphiql.css';
 
 import './fix.css';
 
-const PrettifyButton = () => {
-  const prettify = usePrettifyEditors();
-  return (
-    <ToolbarButton
-      onClick={() => prettify()}
-      title="Prettify Query (Shift-Ctrl-P)"
-      label="Prettify"
-    />
-  );
-};
 
-const HistoryButton = () => {
-  const historyContext = useHistoryContext();
-  return (
-    <ToolbarButton
-      onClick={() => historyContext?.toggle()}
-      title="Show History"
-      label="History"
-    />
-  );
+const API_TYPES = {
+  prod: { label: 'Production' },
+  dev: { label: 'Development' }
 };
 
 const digitransitUrl = (apiType, router) =>
@@ -44,128 +25,138 @@ const graphQLFetcher = (apiType, router) => (graphQLParams) =>
     body: JSON.stringify(graphQLParams)
   }).then((response) => response.json());
 
-class CustomGraphiQL extends React.Component {
-  constructor(props) {
-    super(props);
 
-    const urlSearchParams = new URLSearchParams(props.location.search);
+const isSelected = (config, router) => config.router === router;
 
-    const query =
-      urlSearchParams.has('query') &&
-      decodeURIComponent(urlSearchParams.get('query'));
-    const variables =
-      urlSearchParams.has('variables') &&
-      decodeURIComponent(urlSearchParams.get('variables'));
-    const operationName =
-      urlSearchParams.has('operationName') &&
-      decodeURIComponent(urlSearchParams.get('operationName'));
+const getQueryString = (query, variables, operationName) => {
+  const urlSearchParams = new URLSearchParams();
+  query && urlSearchParams.set('query', encodeURIComponent(query));
+  variables && urlSearchParams.set('variables', encodeURIComponent(variables));
+  operationName &&
+    urlSearchParams.set('operationName', encodeURIComponent(operationName));
 
-    this.state = {
-      query,
-      variables,
-      operationName,
-      apiType:
-        (!!props.location.state && props.location.state.apiType) ||
-        (window.location.hostname === 'api.digitransit.fi' ? 'prod' : 'dev')
-    };
+  return `?${urlSearchParams.toString()}`;
+};
 
-    this.graphiql = React.createRef();
-  }
+const PrettifyButton = () => {
+  const prettify = usePrettifyEditors();
 
-  isSelected = (config) => config.router === this.props.router;
+  return (
+    <GraphiQL.ToolbarButton
+      onClick={prettify}
+      title="Prettify Query (Shift-Ctrl-P)"
+      label="Prettify"
+    />
+  );
+};
 
-  getSearchParams = (query, variables, operationName) => {
-    const urlSearchParams = new URLSearchParams();
-    query && urlSearchParams.set('query', encodeURIComponent(query));
-    variables &&
-      urlSearchParams.set('variables', encodeURIComponent(variables));
-    operationName &&
-      urlSearchParams.set('operationName', encodeURIComponent(operationName));
+const ToggleHistoryButton = () => {
+  const historyCtx = useHistoryContext();
+  return (
+    <GraphiQL.ToolbarButton
+      onClick={() => historyCtx?.toggle()}
+      title="Show History"
+      label="History"
+    />
+  );
+};
 
-    return `?${urlSearchParams.toString()}`;
-  };
+const buildLookup = (arr, propName) =>
+  Array.isArray(arr) &&
+  arr.reduce((acc, x) => ({ ...acc, [x[propName]]: x }), {});
 
-  shouldComponentUpdate(nextProps, nextState) {
-    if (
-      this.state.query !== nextState.query ||
-      this.state.variables !== nextState.variables ||
-      this.state.operationName !== nextState.operationName
-    ) {
-      this.props.replace({
-        search: this.getSearchParams(
-          nextState.query,
-          nextState.variables,
-          nextState.operationName
-        )
-      });
-    }
+const CustomGraphiQL = ({ location, push, configs, replace, router }) => {
+  const [query, setQuery] = useState();
+  const [variables, setVariables] = useState();
+  const [operationName, setOperationName] = useState();
+  const [apiType, setApiType] = useState(
+    (!!location.state && location.state.apiType) ||
+      (window.location.hostname === 'api.digitransit.fi' ? 'prod' : 'dev')
+  );
 
-    return (
-      this.props.location.pathname !== nextProps.location.pathname ||
-      this.state.apiType !== nextState.apiType
-    );
-  }
+  // build { waltti: {router: 'waltti', title: 'Waltti:, ...}} style lookup
+  const endpoints = useMemo(() => buildLookup(configs, 'router'), [configs]);
 
-  render() {
-    return (
-      <GraphiQL
-        ref={this.graphiql}
-        fetcher={graphQLFetcher(this.state.apiType, this.props.router)}
-        query={this.state.query ? this.state.query : undefined}
-        variables={this.state.variables ? this.state.variables : undefined}
-        operationName={
-          this.state.operationName ? this.state.operationName : undefined
-        }
-        onEditQuery={(query) => this.setState({ query })}
-        onEditVariables={(variables) => this.setState({ variables })}
-        onEditOperationName={(operationName) =>
-          this.setState({ operationName })
-        }>
-        <GraphiQL.Toolbar>
-          <PrettifyButton />
-          <HistoryButton />
-          <span style={{ paddingTop: 3 }}>Endpoint:</span>
-          <GraphiQL.Menu label="Endpoint" title="Change GraphQL endpoint">
-            {this.props.configs.map((config) => (
-              <GraphiQL.MenuItem
-                key={config.router}
-                title={config.title}
-                label={config.title}
-                selected={this.isSelected(config)}
-                onSelect={() =>
-                  this.props.push({
-                    pathname: `/${config.router}`,
-                    search: this.getSearchParams(
-                      this.state.query,
-                      this.state.variables,
-                      this.state.operationName
-                    ),
-                    state: { apiType: this.state.apiType }
-                  })
-                }
-              />
-            ))}
-          </GraphiQL.Menu>
-          <span style={{ paddingTop: 3 }}>API version:</span>
-          <GraphiQL.Menu label="API version" title="Change API version">
+  useEffect(() => {
+    const queryString = getQueryString(query, variables, operationName);
+    replace(queryString);
+  }, [replace, query, variables, operationName]);
+
+  useEffect(
+    (location) => {
+      if (!location) {
+        return;
+      }
+
+      const urlSearchParams = new URLSearchParams(location.search);
+
+      const query =
+        urlSearchParams.has('query') &&
+        decodeURIComponent(urlSearchParams.get('query'));
+      const variables =
+        urlSearchParams.has('variables') &&
+        decodeURIComponent(urlSearchParams.get('variables'));
+      const operationName =
+        urlSearchParams.has('operationName') &&
+        decodeURIComponent(urlSearchParams.get('operationName'));
+
+      setQuery(query);
+      setVariables(variables);
+      setOperationName(operationName);
+    },
+    [location]
+  );
+
+  return (
+    <GraphiQL
+      fetcher={graphQLFetcher(apiType, router)}
+      query={query ? query : undefined}
+      variables={variables ? variables : undefined}
+      operationName={operationName ? operationName : undefined}
+      onEditQuery={(query) => setQuery(query)}
+      onEditVariables={(variables) => setVariables(variables)}
+      onEditOperationName={(operationName) => setOperationName(operationName)}>
+      <GraphiQL.Toolbar>
+        <PrettifyButton />
+        <ToggleHistoryButton />
+        <span style={{ paddingTop: 3 }}>Endpoint:</span>
+        <GraphiQL.Menu
+          label={router ? endpoints[router].title : 'Endpoint'}
+          title="Change GraphQL endpoint">
+          {configs.map((config) => (
             <GraphiQL.MenuItem
-              title="Production"
-              label="Production"
-              selected={this.state.apiType === 'prod'}
-              onSelect={() => this.setState({ apiType: 'prod' })}
+              key={config.router}
+              title={config.title}
+              label={config.title}
+              selected={isSelected(config, router)}
+              onSelect={() =>
+                push({
+                  pathname: `/${config.router}`,
+                  search: getQueryString(query, variables, operationName),
+                  state: { apiType: apiType }
+                })
+              }
             />
+          ))}
+        </GraphiQL.Menu>
+        <span style={{ paddingTop: 3 }}>API version:</span>
+        <GraphiQL.Menu
+          label={apiType ? API_TYPES[apiType].label : 'API version'}
+          title="Change API version">
+          {['prod', 'dev'].map((type) => (
             <GraphiQL.MenuItem
-              title="Development"
-              label="Development"
-              selected={this.state.apiType === 'dev'}
-              onSelect={() => this.setState({ apiType: 'dev' })}
+              key={type}
+              title={API_TYPES[type].label}
+              label={API_TYPES[type].label}
+              selected={apiType === type}
+              onSelect={() => setApiType(type)}
             />
-          </GraphiQL.Menu>
-        </GraphiQL.Toolbar>
-      </GraphiQL>
-    );
-  }
-}
+          ))}
+        </GraphiQL.Menu>
+      </GraphiQL.Toolbar>
+    </GraphiQL>
+  );
+};
 
 const GraphiQLRoute = withRouter(({ location, history, router, configs }) => (
   <Route
