1737418f68488c5efe744e1a7eaf4ec4d488e79d
Joona Ojapalo <joona.ojapalo@cgi.fi>
2022-09-05 11:27:54 +0300

    chore: improve test coverage

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+const initWindowLocation = (window, url) => {
+  const location = new URL(url);
+  location.assign = jest.fn();
+  location.replace = jest.fn();
+  location.reload = jest.fn();
+
+  delete window.location;
+  window.location = location;
+};
+
 describe('App', () => {
-  it('should render without crashing', () => {
+  test('renders basepath without crashing', () => {
+    initWindowLocation(window, 'https://localhost/graphiql');
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+  });
+
+  test('renders end-point "hsl" without crashing', () => {
+    initWindowLocation(window, 'https://localhost/graphiql/hsl?query=%257Bfeeds%257BfeedId%257D%257D');
     const div = document.createElement('div');
     ReactDOM.render(<App />, div);
   });
