9ee8623e71c6d2e7f579908d3e64e815f9b15fee
Joel Lappalainen <lappalj8@gmail.com>
2024-11-04 12:18:41 +0200

    Update configuration

--- src/App.js
+++ src/App.js
@@ -8,12 +8,16 @@ import GraphiQL from './GraphiQL';
  * @param {String} apiType 'dev' or 'prod'
  * @param {String} version 'v1' or 'v2'
  * @param {String} router router name (eg. 'hsl', 'waltti', ...)
+ * @param {String} dialect api dialect (eg. 'gtfs' or 'transmodel')
+ * @param {String} dialectVersion version of the api dialect (eg. 'v1')
  * @returns
  */
-const buildUrl = (apiType, version, router) =>
-  `https://${
+const buildUrl = (apiType, version, router, dialect, dialectVersion) => {
+  const apiPath = version === 'v1' ? `routers/${router}/index/graphql` : `${router}/${dialect}/${dialectVersion}`;
+  return `https://${
     apiType === 'dev' ? 'dev-api' : 'api'
-  }.digitransit.fi/routing/${version}/routers/${router}/index/graphql`;
+  }.digitransit.fi/routing/${version}/${apiPath}`;
+}
 
 const VERSION_1 = 'v1';
 const VERSION_2 = 'v2';
@@ -31,10 +35,15 @@ const configs = [
       },
       v2: {
         title: 'HSL (v2)',
-        routerUrl: {
-          dev: buildUrl('dev', VERSION_2, 'hsl'),
-          prod: buildUrl('prod', VERSION_2, 'hsl')
-        }
+        apis: [
+          {
+            title: 'GTFS (v1)',
+            routerUrl: {
+              dev: buildUrl('dev', VERSION_2, 'hsl', 'gtfs', 'v1'),
+              prod: buildUrl('prod', VERSION_2, 'hsl', 'gtfs', 'v1')
+            }
+          }
+        ]
       }
     }
   },
@@ -50,10 +59,15 @@ const configs = [
       },
       v2: {
         title: 'Waltti (v2)',
-        routerUrl: {
-          dev: buildUrl('dev', VERSION_2, 'waltti'),
-          prod: buildUrl('prod', VERSION_2, 'waltti')
-        }
+        apis: [
+          {
+            title: 'GTFS (v1)',
+            routerUrl: {
+              dev: buildUrl('dev', VERSION_2, 'waltti', 'gtfs', 'v1'),
+              prod: buildUrl('prod', VERSION_2, 'waltti', 'gtfs', 'v1')
+            }
+          }
+        ]
       }
     }
   },
@@ -62,10 +76,15 @@ const configs = [
     api: {
       v2: {
         title: 'Waltti alt. (v2)',
-        routerUrl: {
-          dev: buildUrl('dev', VERSION_2, 'waltti-alt'),
-          prod: buildUrl('prod', VERSION_2, 'waltti-alt')
-        }
+        apis: [
+          {
+            title: 'GTFS (v1)',
+            routerUrl: {
+              dev: buildUrl('dev', VERSION_2, 'waltti-alt', 'gtfs', 'v1'),
+              prod: buildUrl('prod', VERSION_2, 'waltti-alt', 'gtfs', 'v1')
+            }
+          }
+        ]
       }
     }
   },
@@ -81,10 +100,15 @@ const configs = [
       },
       v2: {
         title: 'Finland (v2)',
-        routerUrl: {
-          dev: buildUrl('dev', VERSION_2, 'finland'),
-          prod: buildUrl('prod', VERSION_2, 'finland')
-        }
+        apis: [
+          {
+            title: 'GTFS (v1)',
+            routerUrl: {
+              dev: buildUrl('dev', VERSION_2, 'finland', 'gtfs', 'v1'),
+              prod: buildUrl('prod', VERSION_2, 'finland', 'gtfs', 'v1')
+            }
+          }
+        ]
       }
     }
   },
@@ -93,10 +117,15 @@ const configs = [
     api: {
       v2: {
         title: 'VARELY (v2)',
-        routerUrl: {
-          dev: buildUrl('dev', VERSION_2, 'varely'),
-          prod: buildUrl('prod', VERSION_2, 'varely')
-        }
+        apis: [
+          {
+            title: 'GTFS (v1)',
+            routerUrl: {
+              dev: buildUrl('dev', VERSION_2, 'varely', 'gtfs', 'v1'),
+              prod: buildUrl('prod', VERSION_2, 'varely', 'gtfs', 'v1')
+            }
+          }
+        ]
       }
     }
   }
